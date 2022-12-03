1e2962fac61fb5c3f1207fb817e4a7dd640d6fca
Desumond-Osu <ssc051201@gmail.com>
2022-12-03 16:38:01 +0800

    yes day 1

--- Day 1/day1.php
+++ Day 1/day1.php
@@ -1,3 +1,18 @@
 <?php
-
-	
+	if ($file = fopen("input.txt", "r")) {
+		$top = 0;
+		$value = 0;
+		while(!feof($file)) {
+		    $line = fgets($file);
+		    if (is_numeric($line)) {
+		    	$value += $line;
+		    } else {
+		    	if ($value > $top) {
+		    		$top = $value;
+		    	}
+	    		$value = 0;
+		    }
+		}
+		echo $top;
+		fclose($file);
+	}
