2ab6c2530b1f10ed428bf3d6dd336613716f01b7
Desumond-Osu <ssc051201@gmail.com>
2022-12-03 17:09:44 +0800

    day 2

--- Day 1/p1p2.php
+++ Day 1/p1p2.php
@@ -1,19 +1,18 @@
 <?php
-	if ($file = fopen("input.txt", "r")) {
-		$sum = [];
-		$value = 0;
-		while(!feof($file)) {
-		    $line = fgets($file);
-		    if (is_numeric($line)) {
-		    	$value += $line;
-		    } else {
-		    	array_push($sum, $value);
-	    		$value = 0;
-		    }
+	$file = fopen('input.txt', 'r') or die('File error');
+	$sum = [];
+	$value = 0;
+	while(!feof($file)) {
+		$line = fgets($file);
+		if (is_numeric($line)) {
+			$value += $line;
+		} else {
+			array_push($sum, $value);
+			$value = 0;
 		}
-		rsort($sum);
-		echo 'Top 1 > ' . $sum[0];
-		echo "<br/>";
-		echo 'Top 3 > ' . $sum[0] + $sum[1] + $sum[2];
-		fclose($file);
 	}
+	rsort($sum);
+	echo 'Top 1 > ' . $sum[0];
+	echo "<br/>";
+	echo 'Top 3 > ' . $sum[0] + $sum[1] + $sum[2];
+	fclose($file);
--- Day 2/p2.php
+++ Day 2/p2.php
@@ -1,7 +1,7 @@
 <?php
 	$file = fopen('input.txt', 'r') or die('File error');
 	//A > Rock, B > Paper, C > Scissors
-	//X > Rock, Y > Paper, Z > Scissors
+	//X > Lose, Y > Draw, Z > Win
 
 	//1 > Rock, 2 > Paper, 3 > Scissors
 	//0 > Lost, 3 > Draw, 6 > Won
@@ -9,25 +9,25 @@
 	$point = 0;
 	$map = [
 		'A X' => 3,
-		'B Y' => 3,
-		'C Z' => 3,
-		'A Y' => 6,
-		'B Z' => 6,
-		'C X' => 6,
-		'A Z' => 0,
-		'B X' => 0,
-		'C Y' => 0,
+		'A Z' => 2,
+		'A Y' => 1,
+		'B Z' => 3,
+		'B Y' => 2,
+		'B X' => 1,
+		'C Y' => 3,
+		'C X' => 2,
+		'C Z' => 1,
 	];
 
-	$shape = [
-		'X' => 1,
-		'Y' => 2,
-		'Z' => 3,
+	$result = [
+		'X' => 0,
+		'Y' => 3,
+		'Z' => 6,
 	];
 
 	while(!feof($file)) {
 		$line = fgets($file);
-		$point += $map[trim($line)] + $shape[$line[2]];
+		$point += $map[trim($line)] + $result[$line[2]];
 	}
 	echo $point;
 	fclose($file);
